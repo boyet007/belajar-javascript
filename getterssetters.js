86fef65fce357489c090010d6a7b298017be166e
Philip Loa <boyet007@gmail.com>
2018-07-22 13:45:05 +0700

    array, string, getsetters

---
+++ getterssetters.js
@@ -0,0 +1,15 @@
+const person = {
+    firstName : 'Mosh',
+    lastName : 'Hamedenin',
+    get fullName() {
+        return `${person.firstName} ${person.lastName}`;
+    },
+    set fullName(value) {
+        const parts = value.split(' ');
+        this.firstName = parts[0];
+        this.lastName = parts[1];
+    }
+};
+
+person.fullName = 'John Smith';
+console.log(person);
---
+++ string.js
@@ -0,0 +1,6 @@
+//String primitive
+const message = 'hi';
+
+//String object
+const another = new String('hi');
+
